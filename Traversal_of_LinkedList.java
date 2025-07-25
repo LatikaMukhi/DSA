4958f77b5353a70136b482a0dbdb834830e32de3
LatikaMukhi <141509340+LatikaMukhi@users.noreply.github.com>
2025-07-25 11:58:46 +0530

    Traversal_of_LinkedList.java

--- Traversal_of_LinkedList.java
+++ Traversal_of_LinkedList.java
@@ -13,7 +13,7 @@ public class LinkedList {
     private static Node ConvertArr2LL(int[]arr){
         Node head= new Node (arr[0]);
         Node mover = head;
-        for(int i=0; i<arr.length;i++){
+        for(int i=1; i<arr.length;i++){
         Node temp = new Node (arr[i]);
         mover.next = temp;
         mover= temp;
